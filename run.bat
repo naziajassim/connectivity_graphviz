46be1d6d2724a12ab2716b19e789c2e4b63d6056
njass <nazia.jassim@fu-berlin.de>
2017-09-11 20:41:23 +0200

    automate graphviz commands

---
+++ run.bat
@@ -0,0 +1,6 @@
+dot -Kfdp -n contrast_graph_1_2.txt -Tpng -o contrast_graph_1_2.png
+dot -Kfdp -n contrast_graph_3_4.txt -Tpng -o contrast_graph_3_4.png
+dot -Kfdp -n contrast_graph_5_6.txt -Tpng -o contrast_graph_5_6.png
+dot -Kfdp -n contrast_graph_7_8.txt -Tpng -o contrast_graph_7_8.png
+dot -Kfdp -n contrast_graph_5_7.txt -Tpng -o contrast_graph_5_7.png
+dot -Kfdp -n contrast_graph_6_8.txt -Tpng -o contrast_graph_6_8.png
